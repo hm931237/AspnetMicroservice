be8e7348eafbd3c90fcac324b05b6af1d2ea1f7e
hm931237 <hm931237@gmail.com>
2023-08-16 11:29:22 +0300

    Develop GetOrderList Command

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Application.Mappings
 {
@@ -12,7 +8,7 @@ namespace Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            
+            CreateMap<Order, OrdersVm>().ReverseMap();
         }
     }
 }
